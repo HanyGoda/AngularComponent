3451faab44d73481426a6dd17c00663c2cb7b384
HanyGoda <hany2016goda@gmail.com>
2016-11-04 20:54:08 +0200

    FirstCommit

---
+++ AngularComponent/AngularComponent/Controllers/employeeCTRL.js
@@ -0,0 +1,29 @@
+﻿
+myApp.controller("employeeCTRL", function ($scope, Employee, $interval) {
+    var employees = new Array();
+    var interval = null;
+    $scope.empArray = [];
+    Employee.get().then(function (response) {
+        employees = response.data.employees;
+        employees.sort(compare);
+        $scope.empArray.push(employees[0])
+    }), function (response) {
+        console.log(response.statusText);
+    }
+    function compare(a, b) {
+        if (a.Score > b.Score)
+            return -1;
+        if (a.Score < b.Score)
+            return 1;
+        return 0;
+    }
+    interval = $interval(callInterval, 1500);
+    var i = 1;
+    function callInterval() {
+        if (i < employees.length) 
+            $scope.empArray.push(employees[i])
+        else 
+            $interval.cancel(interval);
+        i++;
+    }
+});
---
+++ AngularComponent/AngularComponent/Module/app.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="F:\Hafez\AngularComponent\AngularComponent\scripts/angular.js" />
+
+var myApp = angular.module("myApp", ['ngAnimate']);
+
+myApp.factory("Employee", function ($http) {
+    return {
+        get: function () {
+            return $http.get("../Module/employeeData.json");
+        }
+    };
+});
